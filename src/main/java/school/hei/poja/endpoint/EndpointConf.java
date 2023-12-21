63660ac6e03ddf3ec8df3003f1cf851e79966041
Lakara2 <lbryanlakara@gmail.com>
2023-12-21 09:27:41 +0300

    update CI

--- src/main/java/school/hei/poja/endpoint/EndpointConf.java
+++ src/main/java/school/hei/poja/endpoint/EndpointConf.java
@@ -8,8 +8,4 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class EndpointConf {
-  @Bean
-  public ObjectMapper objectMapper() {
-    return new ObjectMapper().findAndRegisterModules(FAIL_ON_UNKNOWN_PROPERTIES, false);
-  }
 }
