f8260ae66bf209c04ce66de27db2ccc47530420c
Lakara2 <lbryanlakara@gmail.com>
2023-12-21 09:36:18 +0300

    health solv

---
+++ src/main/java/school/hei/poja/endpoint/EndpointConf.java
@@ -0,0 +1,18 @@
+package school.hei.poja.endpoint;
+
+import static com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class EndpointConf {
+    @Configuration
+public class EndpointConf {
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    }
+}
+}
